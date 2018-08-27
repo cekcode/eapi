9feff1687881fde8211964a6cf98c158a1d75eac
cekcode <cekcode@gmail.com>
2018-08-27 20:18:21 +0700

    seeding untuk produck dan review

--- database/migrations/2018_08_26_073133_create_products_table.php
+++ database/migrations/2018_08_26_073133_create_products_table.php
@@ -18,7 +18,7 @@ class CreateProductsTable extends Migration
             $table->string('name');
             $table->text('detail');
             $table->integer('price');
-            $table->integer('stok');
+            $table->integer('stock');
             $table->integer('discount');
             $table->timestamps();
         });
